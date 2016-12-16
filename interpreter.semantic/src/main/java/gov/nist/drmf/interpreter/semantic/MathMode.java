5608940ac6ca3d061f8c5c8c00a53aab672a0d72
Claude Zou <claude.zou.work@gmail.com>
2016-12-16 17:11:39 -0500

    Add more testing code

--- interpreter.semantic/src/main/java/gov/nist/drmf/interpreter/semantic/MathMode.java
+++ interpreter.semantic/src/main/java/gov/nist/drmf/interpreter/semantic/MathMode.java
@@ -3,6 +3,9 @@ package gov.nist.drmf.interpreter.semantic;
 import gov.nist.drmf.interpreter.common.GlobalConstants;
 import mlp.ParseException;
 import mlp.PomParser;
+import mlp.PomTaggedExpression;
+
+import java.util.List;
 
 /**
  * Created by jrp4 on 11/29/16.
@@ -28,11 +31,17 @@ public class MathMode extends LaTeXMode {
         System.out.println(math);
         PomParser parser = new PomParser(GlobalConstants.PATH_REFERENCE_DATA);
         parser.addLexicons(GlobalConstants.DLMF_MACROS_LEXICON_NAME);
+        String output = "";
         try {
-            System.out.println(parser.parse(math));
+            output = parser.parse(math).toString(); //more code for testing
+            List<PomTaggedExpression> comps = parser.parse(math).getComponents();
+            for (int i = 0; i < comps.size(); i++) {
+                System.out.println(comps.get(i));
+            }
         } catch (ParseException e) {
             e.printStackTrace();
         }
+        System.out.println(output);
         return content; //for testing (as of now)
     }
 }
