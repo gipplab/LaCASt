8d39fde81a76aca1a08d4c5e4629b1d8724ef0bf
Claude Zou <claude.zou.work@gmail.com>
2017-01-06 17:24:53 -0500

    Add more code for parsing sty files

--- interpreter.semantic/src/main/java/gov/nist/drmf/interpreter/semantic/Macro.java
+++ interpreter.semantic/src/main/java/gov/nist/drmf/interpreter/semantic/Macro.java
@@ -1,8 +1,14 @@
 package gov.nist.drmf.interpreter.semantic;
 
+import java.util.ArrayList;
+
 /**
  * Created by jrp4 on 1/3/17.
  */
 public class Macro {
+
+    public Macro(ArrayList<String> params) {
+
+    }
 
 }
