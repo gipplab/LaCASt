c5918627ebc81c4cc017156d977681825a8346a4
Jagan Prem <notjagan@gmail.com>
2017-01-06 16:05:25 -0500

    Add basic file reading for .STY parsing

--- interpreter.semantic/src/main/java/gov/nist/drmf/interpreter/semantic/StyFileParser.java
+++ interpreter.semantic/src/main/java/gov/nist/drmf/interpreter/semantic/StyFileParser.java
@@ -1,7 +1,33 @@
 package gov.nist.drmf.interpreter.semantic;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.util.ArrayList;
+
 /**
  * Created by jrp4 on 1/3/17.
  */
 public class StyFileParser {
+
+    public static ArrayList<Macro> parse(Path styFile) {
+        //read from file
+        try {
+            String content = new String(Files.readAllBytes(styFile));
+            return parse(content);
+        } catch (IOException e) {
+            System.out.println("File " + styFile.toString() + " not found.");
+        }
+        return null;
+    }
+
+    public static ArrayList<Macro> parse(String content) {
+        String[] replacements = content.split("\n");
+        ArrayList<Macro> macros = new ArrayList<Macro>();
+        for (int i = 0; i < replacements.length; i++) {
+            //parse function definitions
+        }
+        return macros;
+    }
+
 }
