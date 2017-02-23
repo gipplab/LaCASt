6ad5103563a6edcdf1d5266826e951f6d7ceb78f
AndreG-P <andre.greiner-petter@t-online.de>
2017-02-23 20:20:51 +0100

    Solve maven issues. Maven can compile the project now.

---
+++ interpreter.maple/src/test/java/gov/nist/drmf/interpreter/maple/EngineTests.java
@@ -0,0 +1,15 @@
+package gov.nist.drmf.interpreter.maple;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by AndreG-P on 23.02.2017.
+ */
+public class EngineTests {
+    @Test
+    public void testTest(){
+        assertTrue(true);
+    }
+}
