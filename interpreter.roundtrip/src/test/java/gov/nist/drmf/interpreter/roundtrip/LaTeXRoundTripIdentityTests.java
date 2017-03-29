3b3949173141bf4a4f91f0b39e3c33b2fbb4c1a8
AndreG-P <andre.greiner-petter@t-online.de>
2017-03-29 15:12:37 +0200

    Changed test cases from LaTeX to Maple

--- interpreter.roundtrip/src/test/java/gov/nist/drmf/interpreter/roundtrip/LaTeXRoundTripIdentityTests.java
+++ interpreter.roundtrip/src/test/java/gov/nist/drmf/interpreter/roundtrip/LaTeXRoundTripIdentityTests.java
@@ -1,5 +1,6 @@
 package gov.nist.drmf.interpreter.roundtrip;
 
+import com.maplesoft.externalcall.MapleException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.fail;
@@ -14,23 +15,29 @@ public class LaTeXRoundTripIdentityTests extends AbstractRoundTrip {
     };
 
     @Test
-    void straight() {
-        String test = "x+x^3-\\frac{(\\iunit\\alpha+y)\\cdot 3}{\\left( y^2+x^3 \\right)^z}";
-        test = "(ab^2c13b+2) \\cdot \\CatalansConstant 2";
-        String maple = translator.translateFromLaTeXToMapleClean( test );
-        System.out.println(maple);
-        try {
-            String latex = translator.translateFromMapleToLaTeXClean( maple );
-            String maple2 = translator.translateFromLaTeXToMapleClean( latex );
-            String latex2 = translator.translateFromMapleToLaTeXClean( maple2 );
-            System.out.println(test);
-            System.out.println(maple);
-            System.out.println(latex);
-            System.out.println(maple2);
-            System.out.println(latex2);
-        } catch ( Exception e ){
-            e.printStackTrace();
-            fail();
+    void straight() throws MapleException {
+        boolean latex_equ = false, maple_equ = false;
+        int threshold = 10;
+        String maple, latex, firstMaple, prev_latex, prev_maple;
+        for ( int i = 0; i < latex_test_polynomials.length; i++ ){
+            prev_latex = latex_test_polynomials[i];
+            firstMaple = translator.translateFromLaTeXToMapleClean( prev_latex );
+            prev_maple = firstMaple;
+            for ( int j = 0; j < threshold; j++ ){
+                latex = translator.translateFromMapleToLaTeXClean( prev_maple );
+                maple = translator.translateFromLaTeXToMapleClean( latex );
+                if ( latex.equals( prev_latex ) ){
+                    latex_equ = true;
+                } else if ( translator.simplificationTester(firstMaple, maple) ){
+                    maple_equ = true;
+                }
+                if ( latex_equ && maple_equ ) break;
+                prev_latex = latex;
+                prev_maple = maple;
+            }
+
+            if ( !(latex_equ && maple_equ) )
+                fail( "Round trip test failed for: " + prev_latex );
         }
     }
 }
