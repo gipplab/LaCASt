2572ee1cf4c78c024785cde62f002116f86d4236
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2018-05-01 11:05:30 +0200

    Add alternative cli argument to run only a single test case

--- interpreter.roundtrip/src/main/java/gov/nist/drmf/interpreter/evaluation/NumericalEvaluator.java
+++ interpreter.roundtrip/src/main/java/gov/nist/drmf/interpreter/evaluation/NumericalEvaluator.java
@@ -417,7 +417,15 @@ public class NumericalEvaluator implements Observer {
     public static void main(String[] args) throws Exception{
         NumericalEvaluator evaluator = new NumericalEvaluator();
         evaluator.init();
-        evaluator.loadTestCases();
+        if(args.length>0){
+            evaluator.testCases = new LinkedList<>();
+            evaluator.testCases.add(
+             //Note: Each instantiation of NumericalEvaluator overwrites the static variable labelLinker
+             CaseAnalyzer.analyzeLine(args[0], 0, labelLinker)
+            );
+        } else {
+            evaluator.loadTestCases();
+        }
         evaluator.performAllTests();
         evaluator.writeOutput( evaluator.config.getOutputPath() );
         //System.out.println(evaluator.performSingleTest( evaluator.testCases.getFirst() ));
