daf74c829be1cd20c79e81e7e46cc05397cc6aab
Rajen Dey <rajhataj@gmail.com>
2019-08-06 16:29:04 -0400

    Split test class into 4 test methods. Add some DLMF test cases. Remove none and onlyUpper cases from SumProductTranslator.

--- interpreter.cas/src/test/java/gov/nist/drmf/interpreter/cas/translation/components/SumProductTranslatorTest.java
+++ interpreter.cas/src/test/java/gov/nist/drmf/interpreter/cas/translation/components/SumProductTranslatorTest.java
@@ -1,6 +1,5 @@
 package gov.nist.drmf.interpreter.cas.translation.components;
 
-import gov.nist.drmf.interpreter.cas.SemanticToCASInterpreter;
 import gov.nist.drmf.interpreter.cas.translation.SemanticLatexTranslator;
 import gov.nist.drmf.interpreter.common.GlobalConstants;
 import gov.nist.drmf.interpreter.common.GlobalPaths;
@@ -9,14 +8,12 @@ import gov.nist.drmf.interpreter.common.TeXPreProcessor;
 import gov.nist.drmf.interpreter.mlp.extensions.MacrosLexicon;
 import mlp.PomParser;
 import mlp.PomTaggedExpression;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DynamicTest;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestFactory;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
@@ -57,132 +54,132 @@ public class SumProductTranslatorTest {
             "\n" +
             "Finished conversion to Maple:\n";
 
-    private static final String[] expression = {
+    private static final String[] sums= {
             "\\sum_{x}^{y}{z}", //maple does not handle sums with only variable as lower limit {x} instead of something like {x=0}
-            "\\prod_{x}^{y}{z}", //so the next couple of expressions don't work for maple
             "\\sum^{t}_{y}{z}",
-            "\\prod^{t}_{y}{z}",
             "\\sum_{t}{y}",
-            "\\prod_{t}{y}",
-            "\\sum^{t}{y}", //maple also does not handle sums with only an upper limit
-            "\\prod^{t}{y}",
-            "\\sum{t}",
-            "\\prod{t}",
             "\\sum_{t=0}^{\\infty}{t^2}",
-            "\\prod_{t=0}^{\\infty}{t^2}",
             "\\sum_{t=3}{\\tan{t}}",
-            "\\prod_{t=3}{\\tan{t}}",
             "\\sum^{100}_{t=0}{12}",
-            "\\prod^{100}_{t=0}{12}",
-            "\\prod{A_{2\\sin{u}}}",
+            "\\sum_{x=-\\infty}^{\\infty}x^2(x+2)(y^3-3)-2x+y-2",
             "\\sum_{n=1}^{\\infty}\\frac{(-1)^{n}2^{2n-1}B_{2n}}{n(2n)!}z^{2n}",
-            "\\sum_{t}^{y}{\\sum^{100}{\\prod_{t=3}^{5}{\\sum^{\\infty}_{t=1}{\\prod{3t}}}}}",
-            "\\prod_{t=7}^{\\infty}\\sum^{-72.3z}_{t=-2}\\prod^{12y}{\\frac{2}" +
-                    "{3g\\prod_{q=16\\pi}^{\\infty}{q\\sin{q}}}}\\prod_{h=23}^{\\infty}" +
-                    "\\sum^{z}_{h=\\ln{p^2\\tan{i}}}" + "\\prod_{o=15}^{\\cos{321o+4}}" +
-                    "{\\frac{\\sum{l=2}{y-3}+4}{r\\prod^{\\infty}_{l=y}{l}}} + 3.5",
-            "\\sum 3x46x(3c\\cos{2})(3ln{q})+3x+52",
-            "\\sum_{x=0}4x(y^2)x2t+2tx+3y",
-            "\\prod^{100}\\cos{i}2i(z_{2i^2})+2i^3-2f/3",
-            "\\prod_{x=0}^{\\infty}x\\sin{x^2}\\cos{t}+2sin{4}+3",
             "\\sum^{50}_{r=0}r\\cos{\\Theta}r(3r^2-3)/23x+3q",
-            "\\prod 2x(3y)(3^2)=2x^2(3rt)",
-            "\\sum_{x=0}^{\\infty}23x(x+4)\\prod^{100}2x(x+2)(3y+2)",
-            "(\\sum_{x=0}^{\\infty}2x(y^2-1))(x+2)^2",
             "\\sum_{x=0}^{\\infty}x^3(3x+2y)^{25x^2}(x+2)x^2(x+3)+2x(x+2)^2",
+            //6.6.5
+            "\\sum_{n=0}^\\infty \\frac{\\opminus^n z^{2n+1}}{(2n+1)!(2n+1)}",
+            //29.6.36
+            "\\sum_{p \\hiderel{=} 0}^{\\infty} (2p+1) B_{2p+1}",
+            //22.12.2 part 1
+            "\\sum_{n=-\\infty}^{\\infty}\\frac{\\pi}{\\sin@{\\pi (t - (n+\\frac{1}{2}) \\tau)}}",
+            //22.12.2 part 2
+            "\\sum_{n=-\\infty}^{\\infty} \\left( \\sum_{m=-\\infty}^{\\infty}\\frac{(-1)^m}{t - m - (n+\\frac{1}{2}) \\tau}\\right)",
+            //10.23.4
+            "\\sum_{k \\hiderel{=} 0}^{2n} \\opminus^k \\BesselJ{k}@{z} \\BesselJ{2n-k}@{z}+ 2 \\sum_{k \\hiderel{=} 1}^\\infty \\BesselJ{k}@{z} \\BesselJ{2n+k}@{z}",
+            //10.23.27
+            "\\sum_{k=0}^{n-1} \\frac{(\\tfrac{1}{2} z)^k \\BesselJ{k}@{z}}{k! (n-k)} + \\frac{2}{\\pi} \\left( \\ln@{\\tfrac{1}{2} z} - \\digamma@{n+1} \\right)\\BesselJ{n}@{z}- \\frac{2}{\\pi}\\sum_{k=1}^\\infty \\opminus^k \\frac{(n+2k) \\BesselJ{n+2k}@{z}}{k (n+k)}",
+            //22.12.5 part 1
+            "\\sum_{n=-\\infty}^{\\infty}\\frac{\\pi}{\\sin@{\\pi (t+\\frac{1}{2}-(n+\\frac{1}{2}) \\tau)}}",
+            //22.12.5 part 2
+            "\\sum_{n=-\\infty}^{\\infty} \\left( \\sum_{m=-\\infty}^{\\infty} \\frac{(-1)^m}{t + \\frac{1}{2} - m - (n+\\frac{1}{2}) \\tau}\\right)",
+            //35.7.3
+            "\\sum_{k=0}^\\infty\\frac{\\Pochhammersym{a}{k} \\Pochhammersym{c-a}{k}\\Pochhammersym{b}{k} \\Pochhammersym{c-b}{k}}{k! \\, \\Pochhammersym{c}{2k} \\Pochhammersym{c-\\tfrac{1}{2}}{k}}(t_1 t_2)^k",
+            //25.16.11
+            "\\sum_{n=1}^\\infty \\frac{1}{n^s} \\sum_{m=1}^n \\frac{1}{m^z}",
+            //18.2.6
+            "\\sum_{x \\in X} x \\left( p_n(x) \\right)^2 w_x",
 
 
 
     };
 
-    private static final String[] translatedMaple = {
+    private static final String[] prods = {
+            "\\prod_{x}^{y}{z}",
+            "\\prod^{t}_{y}{z}",
+            "\\prod_{t}{y}",
+            "\\prod_{t=0}^{\\infty}{t^2}",
+            "\\prod_{t=3}{\\tan{t}}",
+            "\\prod^{100}_{t=0}{12}",
+            "\\prod_{x=0}^{\\infty}x\\sin{x^2}\\cos{t}+2sin{4}+3",
+    };
+
+    private static final String[] translatedMapleSums = {
             "sum(z, x..y)",
-            "product(z, x..y)",
             "sum(z, y..t)",
-            "product(z, y..t)",
             "sum(y, t)",
-            "product(y, t)",
-            "\"errorsequence\"",
-            "\"errorsequence\"",
-            "sum(t, i)",
-            "product(t, i)",
             "sum((t)^(2), t = 0..infinity)",
-            "product((t)^(2), t = 0..infinity)",
             "sum(tan(t), t = 3)",
-            "product(tan(t), t = 3)",
             "sum(12, t = 0..100)",
+            "sum(x^(2)*(x + 2)((y)^(3) - 3)-2x, x = - infinity..infinity)+ y - 2",
+            "sum(((- 1)^(n)* (2)^(2*n - 1)* B[2*n])/(n*factorial((2*n)))z^(2*n), n = 1..infinity)",
+            "sum(rcos(Theta)r(3*(r)^(2) - 3)/23x, r = 0..50)+ 3*q",
+            "sum(x^(3)*(3*x + 2*y)^(25*(x)^(2))*(x + 2)x^(2)*(x + 3)+2x(x + 2)^(2), x = 0..infinity)",
+            "sum(((-1)^(n)* (z)^(2*n + 1))/(factorial((2*n + 1))*(2*n + 1)), n = 0..infinity)",
+            "sum((2*p + 1)B[2*p + 1], p = 0..infinity)",
+            "sum((pi)/(sin(pi*(t -(n +(1)/(2))tau))), n = - infinity..infinity)",
+            "sum((sum(((- 1)^(m))/(t - m -(n +(1)/(2))*tau), m = - infinity..infinity)), n = - infinity..infinity)",
+            "sum((-1)^(k)*BesselJ(k, z)BesselJ(2*n - k, z), k = 0..2*n)+2sum(BesselJ(k, z)BesselJ(2*n + k, z), k = 1..infinity)",
+            "sum((((1)/(2)*z)^(k)* BesselJ(k, z))/(factorial(k)*(n - k)), k = 0..n - 1)+(2)/(pi)(ln((1)/(2)*z) - Psi(n + 1))BesselJ(n, z)-(2)/(pi)sum((-1)^(k)*((n + 2*k)*BesselJ(n + 2*k, z))/(k*(n + k)), k = 1..infinity)",
+            "sum((pi)/(sin(pi*(t +(1)/(2)-(n +(1)/(2))tau))), n = - infinity..infinity)",
+            "sum((sum(((- 1)^(m))/(t +(1)/(2)- m -(n +(1)/(2))*tau), m = - infinity..infinity)), n = - infinity..infinity)",
+            "sum((pochhammer(a, k)*pochhammer(c - a, k)*pochhammer(b, k)*pochhammer(c - b, k))/(factorial(k)*pochhammer(c, 2*k)*pochhammer(c -(1)/(2), k))(t[1] t[2])^(k), k = 0..infinity)",
+            "sum((1)/((n)^(s))sum((1)/((m)^(z)), m = 1..n), n = 1..infinity)",
+            "sum(x(p[n](x))^(2)*w[x], x in X)",
+
+    };
+
+    private static final String[] translatedMapleProds = {
+            "product(z, x..y)",
+            "product(z, y..t)",
+            "product(y, t)",
+            "product((t)^(2), t = 0..infinity)",
+            "product(tan(t), t = 3)",
             "product(12, t = 0..100)",
-            "cos(sum(- 2*ln(q), t = - 12..24*y))",
-            "product(A[2*sin(u)], A)",
-            "(1)/(z)+ 2*z*sum((1)/((z)^(2)- (n)^(2)* (pi)^(2)), n = 1..infinity)",
-            "sum(((- 1)^(n)* (2)^(2*n - 1)* B[2*n])/(n*factorial((2*n))), n = 1..infinity)*(z)^(2*n)",
-            "(2*p)/((y)^(2))+ ln(2*(3 + (p)^(2)))+ product([(t)^(2)* sin(t)], t = 0..infinity)+ (2)^(t)* cos(y)",
-            "(2*p)/((y)^(2))+ ln(2*(3 + (p)^(2)))+ product(((t)^(2)* sin(x)), x = 0..infinity)+ (2)^(t)* cos(y)",
-            "z[product(1/ (t)^(3), t = 1..infinity)]",
-            "^(\"errorsequence\")+(2*tan(sum(product(2 + (t)^(3), t = 0..63), i)+ 5*f))/(37)",
-            "sum(\"errorsequence\", t..y)",
-            "q[j] = gamma[j] sum((1)/(z[k] - a[j]), k = 1..n)",
-            "\"errorsequence\"",
-            "((b)^(2))*sin((b)^(2))+ 4*arccos(product(12, b = 1)*(y)^(p)* log(b))+(\"errorsequence\")/(1.2*n)",
-            "((b)^(2))*sin((b)^(2))+ 4*arccos(product(12, b = 1)*(y)^(p)* log(b))+(b*sum(3*cos(b), b = 1..200))/(1.2*n)",
-            "((d)^(2)* w)/(d*(z)^(2))+(sum((gamma[j])/(z - a[j]), j = 1..N))*(d*w)/(d*z)+(Phi*(z))/(product((z - a[j]), j = 1..N))*w = 0"
+            "product(xsin((x)^(2))cos(t), x = 0..infinity)+ 2*sin(4)+ 3",
+
     };
 
-    private static final String[] translatedMathematica = {
+    private static final String[] translatedMathematicaSums = {
             "Sum[z, {x, x, y}]",
-            "Product[z, {x, x, y}]",
             "Sum[z, {y, y, t}]",
-            "Product[z, {y, y, t}]",
             "Sum[y, t]",
-            "Product[y, t]",
-            "Sum[y, {y, t}]",
-            "Product[y, {y, t}]",
-            "Sum[t, t]",
-            "Product[t, t]",
             "Sum[(t)^(2), {t, 0, Infinity}]",
-            "Product[(t)^(2), {t, 0, Infinity}]",
             "Sum[Tan[t], t = 3]",
-            "Product[Tan[t], t = 3]",
             "Sum[12, {t, 0, 100}]",
-            "Product[12, {t, 0, 100}]",
-            "Product[Subscript[A, 2 Sin[u]], A]",
+            "Sum[x^(2) (x + 2)((y)^(3) - 3)-2x, {x, -Infinity, Infinity}] + y - 2",
             "Sum[Divide[(- 1)^(n)  (2)^(2 n - 1)  Subscript[B, 2 n],n (2 n)!]z^(2 n), {n, 1, Infinity}]",
-            "Sum[Sum[Product[Sum[Product[3 t, t], {t, 1, Infinity}], {t, 3, 5}], {t, 100}], {t, t, y}]",
-            "Product[Sum[Product[Divide[2,3 g Product[q Sin[q], {q, q = 16 \\[Pi], Infinity}]], {i, 12 y}], {t, t = - 2, - 72.3 z}], {t, t = 7, Infinity}] Product[Sum[Product[Divide[Sum[l = 2, l] y - 3+ 4,r Product[l, {l, l = y, Infinity}]], {o, o = 15, Cos[321 o + 4]}], {h, h = Log[(p)^(2)  Tan[i]], z}], {h, h = 23, Infinity}] + 3.5",
-            "Sum[3x 4 6 x(3 c Cos[2])(3 ln(q)), x] + 3 x + 52",
-            "Sum[4x((y)^(2))x 2 t, x = 0] + 2 t x + 3 y",
-            "Product[Cos[i]2i(Subscript[z, 2 (i)^(2)]), {i, 100}] + 2 (i)^(3)- 2 f/ 3",
-            "Product[xSin[(x)^(2)]Cos[t], {x, x = 0, Infinity}] + 2 sin(4)+ 3",
-            "Sum[rCos[\\[CapitalTheta]]r(3 (r)^(2) - 3)/23x, {r, r = 0, 50}] + 3 q",
-            "Product[2x(3 y)((3)^(2)), x] = 2 (x)^(2)  (3 r t)",
-            "Sum[23x(x + 4)Product[2x(x + 2)(3 y + 2), {x, 100}], {x, 0, Infinity}]",
-            "(Sum[2x((y)^(2) - 1), {x, x = 0, Infinity}]) (x + 2)^(2)",
-            "Sum[x^(3) (3 x + 2 y)^(25 (x)^(2)) (x + 2)x^(2) (x + 3), {x, x = 0, Infinity}] + 2 x (x + 2)^(2)"
-
+            "Sum[rCos[\\[CapitalTheta]]r(3 (r)^(2) - 3)/23x, {r, 0, 50}] + 3 q",
+            "Sum[x^(3) (3 x + 2 y)^(25 (x)^(2)) (x + 2)x^(2) (x + 3)+2x(x + 2)^(2), {x, 0, Infinity}]",
+            "Sum[Divide[(-1)^(n)  (z)^(2 n + 1),(2 n + 1)!(2 n + 1)], {n, 0, Infinity}]",
+            "Sum[(2 p + 1)Subscript[B, 2 p + 1], {p, 0, Infinity}]",
+            "Sum[Divide[\\[Pi],Sin[\\[Pi] (t -(n +Divide[1,2])\\[Tau])]], {n, -Infinity, Infinity}]",
+            "Sum[(Sum[Divide[(- 1)^(m),t - m -(n +Divide[1,2]) \\[Tau]], {m, -Infinity, Infinity}]), {n, -Infinity, Infinity}]",
+            "Sum[(-1)^(k) BesselJ[k, z]BesselJ[2 n - k, z], {k, 0, 2 n}]+2Sum[BesselJ[k, z]BesselJ[2 n + k, z], {k, 1, Infinity}]",
+            "Sum[Divide[(Divide[1,2] z)^(k)  BesselJ[k, z],k!(n - k)], {k, 0, n - 1}]+Divide[2,\\[Pi]](Log[Divide[1,2] z] - PolyGamma[n + 1])BesselJ[n, z]-Divide[2,\\[Pi]]Sum[(-1)^(k) Divide[(n + 2 k) BesselJ[n + 2 k, z],k (n + k)], {k, 1, Infinity}]",
+            "Sum[Divide[\\[Pi],Sin[\\[Pi] (t +Divide[1,2]-(n +Divide[1,2])\\[Tau])]], {n, -Infinity, Infinity}]",
+            "Sum[(Sum[Divide[(- 1)^(m),t +Divide[1,2]- m -(n +Divide[1,2]) \\[Tau]], {m, -Infinity, Infinity}]), {n, -Infinity, Infinity}]",
+            "Sum[Divide[Pochhammer[a, k] Pochhammer[c - a, k] Pochhammer[b, k] Pochhammer[c - b, k],k! Pochhammer[c, 2 k] Pochhammer[c -Divide[1,2], k]](Subscript[t, 1] Subscript[t, 2])^(k), {k, 0, Infinity}]",
+            "Sum[Divide[1,(n)^(s)]Sum[Divide[1,(m)^(z)], {m, 1, n}], {n, 1, Infinity}]",
+            "Sum[x(Subscript[p, n](x))^(2) Subscript[w, x], x \\[Element] X]",
 
     };
 
-    private static final String[] removedTestCases = {
-            "\\cos{\\sum_{t=-12}^{24y}{-2\\ln{q}}}",
-            "\\frac{1}{z}+2z\\sum_{n=1}^{\\infty}\\frac{1}{z^{2}-n^{2}\\pi^{2}}",
-            "\\frac{2p}{y^2}+\\ln{2(3+p^2)}+\\prod^{\\infty}_{t=0}[t^2\\sin{t}]+2^t\\cos{y}",
-            "\\frac{2p}{y^2}+\\ln{2(3+p^2)}+\\prod^{\\infty}_{x=0}(t^2\\sin{x})+2^t\\cos{y}",
-            "z_{\\prod_{t=1}^{\\infty}{1/t^3}}",
-            "z^{\\sum^{100}{u}{t^2+2_v}+3}+\\frac{2\\tan{\\sum{\\prod_{t=0}^{63}{2+t^3}}+5f}}{37}",
-            "q_{j}=\\gamma_{j}\\sum_{k=1}^{n}\\frac{1}{z_{k}-a_{j}}",
-            "(b^2)\\sin{b^2}+4\\arccos{\\prod_{b=1}{12}{y^p\\log{b}}}+\\frac{b\\sum^{200}{3\\cos{b}}}{1.2n}",            //and this
-            "(b^2)\\sin{b^2}+4\\arccos{\\prod_{b=1}{12}{y^p\\log{b}}}+\\frac{b\\sum^{200}_{b=1}{3\\cos{b}}}{1.2n}",
-            "\\frac{{\\mathrm{d}}^{2}w}{{\\mathrm{d}z}^{2}}+\\left(\\sum_{j=1}^{N}\\frac{\\gamma_{j}}{z-a_{j}}\\right)\\frac{\\mathrm{d}w}{\\mathrm{d}z}+\\frac{\\Phi(z)}{\\prod_{j=1}^{N}(z-a_{j})}w=0",
-            "\\sum_{j=1}^{N}\\frac{\\gamma_{j}}{t_{k}-a_{j}}+\\sum_{j=1}^{n-1}\\frac{1}{t_{k}-z_{j}^{\\prime}}=0",
-            //a_b^c gives error        "\\sum_{j=1}^{N}\\frac{\\gamma_{j}}{t_{k}-a_{j}}+\\sum_{j=1}^{n-1}\\frac{1}{t_{k}-z_{j}^{\\prime}}=0",
-            // subarrays don't work     "\\sum_{j=1}^{N}\\frac{\\gamma_{j}/2}{z_{k}-a_{j}}+\\sum_{\\begin{subarray}{c}j=1\\\\ j\\neq k\\end{subarray}}^{n}\\frac{1}{z_{k}-z_{j}}=0",
+    private static final String[] translatedMathematicaProds = {
+            "Product[z, {x, x, y}]",
+            "Product[z, {y, y, t}]",
+            "Product[y, t]",
+            "Product[(t)^(2), {t, 0, Infinity}]",
+            "Product[Tan[t], t = 3]",
+            "Product[12, {t, 0, 100}]",
+            "Product[xSin[(x)^(2)]Cos[t], {x, 0, Infinity}] + 2 sin(4)+ 3",
+
     };
 
-    private static ByteArrayOutputStream result;
     private static SemanticLatexTranslator slt;
+    private static PomParser parser;
+    private static SumProductTranslator spt;
 
     @BeforeAll
-    public static void setUp() {
+    private static void setUp(){
         GlobalConstants.CAS_KEY = Keys.KEY_MATHEMATICA;
         slt = new SemanticLatexTranslator(Keys.KEY_LATEX, Keys.KEY_MATHEMATICA);
         try {
@@ -190,19 +187,88 @@ public class SumProductTranslatorTest {
         } catch (IOException e) {
             throw new RuntimeException();
         }
-        result = new ByteArrayOutputStream();
-//        System.setOut(new PrintStream(result));
+        parser = new PomParser(GlobalPaths.PATH_REFERENCE_DATA);
+        parser.addLexicons(MacrosLexicon.getDLMFMacroLexicon());
+        spt = new SumProductTranslator();
     }
+
 
     @TestFactory
     Stream<DynamicTest>  sumMathematicaTest() {
-        PomParser parser = new PomParser(GlobalPaths.PATH_REFERENCE_DATA.toString());
-        parser.addLexicons( MacrosLexicon.getDLMFMacroLexicon() );
+        List<String> expressions = Arrays.asList(sums);
+        List<String> output = Arrays.asList(translatedMathematicaSums);
+        return expressions
+                .stream()
+                .map(
+                        exp -> DynamicTest.dynamicTest("Expression: " + exp, () -> {
+                            int index = expressions.indexOf(exp);
+                            PomTaggedExpression ex = parser.parse(TeXPreProcessor.preProcessingTeX(expressions.get(index)));
 
-        SumProductTranslator spt = new SumProductTranslator();
+                            List<PomTaggedExpression> components = ex.getComponents();
+                            PomTaggedExpression first = components.remove(0);
+                            spt.translate(first, components);
+                            assertEquals(output.get(index), spt.getTranslation());
+                        }));
+    }
 
-        List<String> expressions = Arrays.asList(expression);
-        List<String> output = Arrays.asList(translatedMathematica);
+    @TestFactory
+    Stream<DynamicTest>  prodMathematicaTest() {
+        List<String> expressions = Arrays.asList(prods);
+        List<String> output = Arrays.asList(translatedMathematicaProds);
+
+        return expressions
+                .stream()
+                .map(
+                        exp -> DynamicTest.dynamicTest("Expression: " + exp, () -> {
+                            int index = expressions.indexOf(exp);
+                            PomTaggedExpression ex = parser.parse(TeXPreProcessor.preProcessingTeX(expressions.get(index)));
+
+                            List<PomTaggedExpression> components = ex.getComponents();
+                            PomTaggedExpression first = components.remove(0);
+                            spt.translate(first, components);
+                            assertEquals(output.get(index), spt.getTranslation());
+                        }));
+    }
+
+
+    @TestFactory
+    Stream<DynamicTest>  sumMapleTest() {
+        GlobalConstants.CAS_KEY = Keys.KEY_MAPLE;
+        slt = new SemanticLatexTranslator(Keys.KEY_LATEX, Keys.KEY_MAPLE);
+        try {
+            slt.init(GlobalPaths.PATH_REFERENCE_DATA);
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+
+        List<String> expressions = Arrays.asList(sums);
+        List<String> output = Arrays.asList(translatedMapleSums);
+
+        return expressions
+                .stream()
+                .map(
+                        exp -> DynamicTest.dynamicTest("Expression: " + exp, () -> {
+                            int index = expressions.indexOf(exp);
+                            PomTaggedExpression ex = parser.parse(TeXPreProcessor.preProcessingTeX(expressions.get(index)));
+
+                            List<PomTaggedExpression> components = ex.getComponents();
+                            PomTaggedExpression first = components.remove(0);
+                            spt.translate(first, components);
+                            assertEquals(output.get(index), spt.getTranslation());
+                        }));
+    }
+
+    @TestFactory
+    Stream<DynamicTest>  prodMapleTest() {
+        GlobalConstants.CAS_KEY = Keys.KEY_MAPLE;
+        slt = new SemanticLatexTranslator(Keys.KEY_LATEX, Keys.KEY_MAPLE);
+        try {
+            slt.init(GlobalPaths.PATH_REFERENCE_DATA);
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+        List<String> expressions = Arrays.asList(prods);
+        List<String> output = Arrays.asList(translatedMapleProds);
 
         return expressions
                 .stream()
