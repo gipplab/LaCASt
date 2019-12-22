403fbca535dd8931e2094e40e934959782c29b87
AndreG-P <andre.greiner-petter@t-online.de>
2019-12-23 05:07:13 +0900

    Minor changes to numerical tests

--- interpreter.roundtrip/src/main/java/gov/nist/drmf/interpreter/MathematicaTranslator.java
+++ interpreter.roundtrip/src/main/java/gov/nist/drmf/interpreter/MathematicaTranslator.java
@@ -9,7 +9,9 @@ import gov.nist.drmf.interpreter.common.exceptions.ComputerAlgebraSystemEngineEx
 import gov.nist.drmf.interpreter.common.exceptions.TranslationException;
 import gov.nist.drmf.interpreter.common.grammar.IComputerAlgebraSystemEngine;
 import gov.nist.drmf.interpreter.constraints.IConstraintTranslator;
+import gov.nist.drmf.interpreter.evaluation.ICASEngineNumericalEvaluator;
 import gov.nist.drmf.interpreter.evaluation.ICASEngineSymbolicEvaluator;
+import gov.nist.drmf.interpreter.evaluation.INumericalEvaluationScripts;
 import gov.nist.drmf.interpreter.mathematica.MathematicaInterface;
 import gov.nist.drmf.interpreter.mathematica.evaluate.SymbolicEquivalenceChecker;
 import org.apache.logging.log4j.LogManager;
@@ -26,7 +28,8 @@ import java.util.regex.Pattern;
 public class MathematicaTranslator implements
         IConstraintTranslator,
         IComputerAlgebraSystemEngine<String>,
-        ICASEngineSymbolicEvaluator<Expr>
+        ICASEngineSymbolicEvaluator<Expr>,
+        ICASEngineNumericalEvaluator<Expr>
 {
     private static final Logger LOG = LogManager.getLogger(MathematicaTranslator.class.getName());
 
@@ -126,5 +129,59 @@ public class MathematicaTranslator implements
     @Override
     public void update(Observable o, Object arg) {
         // nothing to do here
+    }
+
+    /**
+     * Mathematica Numerical Tests Workflow:
+     *
+     * 1) Get Variables in Expression
+     * 2) In case of Constraints
+     *  2.1) ConstVars from Variable
+     *  2.2) Get ConstVars - Value Pairs
+     * 3) In Case of special values
+     *  3.1) Special Variables from Variables
+     *  3.2) Special Variables - Value Pairs
+     * 4) Rest of Variables - Value pairs
+     * 1) Variables - Values paris
+     */
+
+    @Override
+    public String storeVariables(String expression, List<String> testValues) {
+        return null;
+    }
+
+    @Override
+    public String storeConstraintVariables(String variableName, List<String> constraintVariables, List<String> constraintValues) {
+        // 1) variableName := Complement[variableName, contVars]
+        // 2)
+
+        // first, get constraint variables that are actually in var
+        // Intersection[list1,list2];
+        return null;
+    }
+
+    @Override
+    public String storeExtraVariables(String variableName, List<String> extraVariables, List<String> extraValues) {
+        return null;
+    }
+
+    @Override
+    public String setConstraints(List<String> constraints) {
+        return null;
+    }
+
+    @Override
+    public String buildTestCases(String constraintsName, String variableNames, String constraintVariableNames, String extraVariableNames, int maxCombis) throws ComputerAlgebraSystemEngineException, IllegalArgumentException {
+        return null;
+    }
+
+    @Override
+    public Expr performNumericalTests(String expression, String testCasesName, String postProcessingMethodName, int precision) throws ComputerAlgebraSystemEngineException {
+        return null;
+    }
+
+    @Override
+    public ResultType getStatusOfResult(Expr results) throws ComputerAlgebraSystemEngineException {
+        return null;
     }
 }
