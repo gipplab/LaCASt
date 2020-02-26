bf42b4690299289fc58979a7a9d83b86c7efc79e
AndreG-P <andre.greiner-petter@t-online.de>
2020-02-27 02:05:25 +0900

    Fix travis code coverage

--- .travis/fix-jacoco.sh
+++ .travis/fix-jacoco.sh
@@ -3,4 +3,8 @@
 JACOCO_FILE="target/jacoco-report/jacoco.xml"
 JACOCO_REPL_FILE="target/jacoco-report/jacoco-fix.xml"
 
+echo $(ls "target/jacoco-report")
+echo "Start replacing..."
 sed -e 's/gov\//src\/main\/java\/gov\//g' -e 's/<[\/]\?group[^>]*>//g' $JACOCO_FILE > $JACOCO_REPL_FILE
+echo "Finished replacing"
+echo $(ls "target/jacoco-report")
