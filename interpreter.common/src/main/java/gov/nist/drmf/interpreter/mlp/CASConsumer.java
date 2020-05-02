dfd042689a240e05014d0742e07910d56d1867d6
AndreG-P <andre.greiner-petter@t-online.de>
2020-05-02 18:28:14 +0900

    Add test cases for lexicon generator

--- interpreter.common/src/main/java/gov/nist/drmf/interpreter/mlp/CASConsumer.java
+++ interpreter.common/src/main/java/gov/nist/drmf/interpreter/mlp/CASConsumer.java
@@ -1,16 +1,10 @@
 package gov.nist.drmf.interpreter.mlp;
 
-import gov.nist.drmf.interpreter.common.constants.Keys;
 import gov.nist.drmf.interpreter.mlp.data.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Arrays;
-import java.util.regex.Matcher;
 
 /**
  * @author Andre Greiner-Petter
@@ -39,33 +33,39 @@ public class CASConsumer implements LexiconInfoConsumer {
             lineAnalyzer.setLine(elements);
             String curr_cas = lineAnalyzer.getCasPrefix();
 
-            cas_func = lineAnalyzer.getValue( curr_cas );
+            cas_func = lineAnalyzer.getValue(curr_cas);
             holder = LexiconConverterUtility.getFuncNameAndFillInteger(
                     cas_func,
                     "Skip cache entry: " + cas_func,
                     lineAnalyzer
             );
 
-            if ( holder == null ) return;
+            if (holder == null) return;
 
             CASInfo info = new CASInfo();
-            info.constraints = lineAnalyzer.getValue(
-                    DLMFTranslationHeaders.cas_constraint.getCSVKey(curr_cas) );
-            info.branch_cuts = lineAnalyzer.getValue(
-                    DLMFTranslationHeaders.cas_branch_cuts.getCSVKey(curr_cas) );
-            info.link = lineAnalyzer.getValue(
-                    DLMFTranslationHeaders.cas_link.getCSVKey(curr_cas) );
+            info.setConstraints(
+                    lineAnalyzer.getValue(DLMFTranslationHeaders.cas_constraint.getCSVKey(curr_cas))
+            );
+            info.setBranch_cuts(
+                    lineAnalyzer.getValue(DLMFTranslationHeaders.cas_branch_cuts.getCSVKey(curr_cas))
+            );
+            info.setLink(
+                    lineAnalyzer.getValue(DLMFTranslationHeaders.cas_link.getCSVKey(curr_cas))
+            );
+            info.setExtra_package(
+                    lineAnalyzer.getValue(DLMFTranslationHeaders.cas_package.getCSVKey(curr_cas))
+            );
 
-            if (info.extra_package != null && !info.extra_package.isEmpty())
-                LOG.debug("EXTRA PACKAGE: " + info.extra_package);
+            if (info.getExtra_package() != null && !info.getExtra_package().isEmpty())
+                LOG.debug("EXTRA PACKAGE: " + info.getExtra_package());
 
-            if ( holder.getCasName() != null )
-                cache.add( holder.getCasName(), holder.getNumVars(), info );
-        } catch ( NumberFormatException nfe ){
+            if (holder.getCasName() != null)
+                cache.add(holder.getCasName(), holder.getNumVars(), info);
+        } catch (NumberFormatException nfe) {
             LOG.debug("Skip cache entry, because number of variables is missing for: " + cas_func);
-        } catch ( NullPointerException npe ){
+        } catch (NullPointerException npe) {
             LOG.debug("Skip cache entry, caused by missing information: " + Arrays.toString(elements));
-        } catch ( Exception e ){
+        } catch (Exception e) {
             LOG.debug("Error - Skip cache entry for: " + Arrays.toString(elements), e);
         }
     }
--- interpreter.common/src/main/java/gov/nist/drmf/interpreter/mlp/data/CASInfo.java
+++ interpreter.common/src/main/java/gov/nist/drmf/interpreter/mlp/data/CASInfo.java
@@ -4,8 +4,42 @@ package gov.nist.drmf.interpreter.mlp.data;
  * Created by AndreG-P on 04.04.2017.
  */
 public class CASInfo {
-    public String link = "",
+    private String link = "",
             constraints = "",
             branch_cuts = "",
             extra_package = "";
+
+    public CASInfo() {}
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getConstraints() {
+        return constraints;
+    }
+
+    public void setConstraints(String constraints) {
+        this.constraints = constraints;
+    }
+
+    public String getBranch_cuts() {
+        return branch_cuts;
+    }
+
+    public void setBranch_cuts(String branch_cuts) {
+        this.branch_cuts = branch_cuts;
+    }
+
+    public String getExtra_package() {
+        return extra_package;
+    }
+
+    public void setExtra_package(String extra_package) {
+        this.extra_package = extra_package;
+    }
 }
