cc9ef37b12d0771aa7720b553ba0341d4e71ad76
AndreG-P <andre.greiner-petter@t-online.de>
2020-06-24 15:42:41 +0900

    Fixing deployment

--- .travis/dke-deploy.sh
+++ .travis/dke-deploy.sh
@@ -6,7 +6,9 @@ chmod 600 ~/.ssh/dke-travis-git
 ssh-add ~/.ssh/dke-travis-git
 
 # deploy to DKE server
+git switch -c deploy-branch
+git commit -am "Add updated version from Travis"
 git remote add deploy ssh://git@$DEPLOY_IP:$DEPLOY_PORT$DEPLOY_DIR
-git push deploy master
+git push deploy deploy-branch
 
 echo "Successfully deployed to DKE server"
