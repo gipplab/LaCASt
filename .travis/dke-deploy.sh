5c91b063df265f42d143065613be98007a19c8a4
AndreG-P <andre.greiner-petter@t-online.de>
2020-10-21 16:29:36 +0900

    Let's try to push to master directly... maybe that helps

--- .travis/dke-deploy.sh
+++ .travis/dke-deploy.sh
@@ -7,12 +7,9 @@ ssh-add ~/.ssh/dke-travis-git
 
 # deploy to DKE server
 git fetch --unshallow origin || true
-git checkout -b deploy-branch
-git add .
 git commit -am "Add updated version from Travis"
 git remote add deploy ssh://git@$DEPLOY_IP:$DEPLOY_PORT$DEPLOY_DIR
-
-if git push -f deploy deploy-branch; then
+if git push -f deploy master; then
   echo "Successfully deployed to DKE server"
 else
   echo "Unable to deploy to DKE server"
