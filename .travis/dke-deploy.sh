788d9ff26591540dfffc9c62de4adf25fbcd15c5
AndreG-P <andre.greiner-petter@t-online.de>
2020-10-21 15:54:46 +0900

    Force changes to DKE in case of conflicts. Nobody should touch DKE branches. Hence, conflicts should only appear if somebody made a mistake on DKE right? In this case, we simply overwrite these errors by forcing git push our new version to DKE 'git push -f deploy deploy-branch'. Let's see if it works now.

--- .travis/dke-deploy.sh
+++ .travis/dke-deploy.sh
@@ -6,14 +6,13 @@ chmod 600 ~/.ssh/dke-travis-git
 ssh-add ~/.ssh/dke-travis-git
 
 # deploy to DKE server
-git fetch --unshallow || true
+git fetch --unshallow origin || true
 git checkout -b deploy-branch
+git add .
 git commit -am "Add updated version from Travis"
 git remote add deploy ssh://git@$DEPLOY_IP:$DEPLOY_PORT$DEPLOY_DIR
-git pull deploy deploy-branch
-git push deploy deploy-branch
 
-if git push deploy deploy-branch; then
+if git push -f deploy deploy-branch; then
   echo "Successfully deployed to DKE server"
 else
   echo "Unable to deploy to DKE server"
