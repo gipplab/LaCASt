00c2221993542d548f64be641f393c202bf648e8
AndreG-P <andre.greiner-petter@t-online.de>
2020-11-17 01:25:04 +0900

    Add new generic replacements to existing MLP pipeline

--- interpreter.generic/src/main/java/gov/nist/drmf/interpreter/generic/pojo/SemanticEnhancedDocument.java
+++ interpreter.generic/src/main/java/gov/nist/drmf/interpreter/generic/pojo/SemanticEnhancedDocument.java
@@ -1,8 +1,11 @@
 package gov.nist.drmf.interpreter.generic.pojo;
 
 import gov.nist.drmf.interpreter.generic.mlp.struct.MLPDependencyGraph;
+import gov.nist.drmf.interpreter.generic.mlp.struct.MOIAnnotation;
 import gov.nist.drmf.interpreter.generic.mlp.struct.MOIPresentations;
+import gov.nist.drmf.interpreter.pom.moi.MOINode;
 
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,6 +24,7 @@ public class SemanticEnhancedDocument {
 
     public SemanticEnhancedDocument(MLPDependencyGraph graph) {
         this.formulae = graph.getVertices().stream()
+                .sorted(Comparator.comparing(MOINode::getAnnotation))
                 .map(MOIPresentations::new)
                 .collect(Collectors.toList());
     }
