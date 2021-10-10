b886e5ae8f812065aaaf0c40cc597fbb9b1519b8
AndreG-P <andre.greiner-petter@t-online.de>
2021-10-10 17:38:33 +0900

    Fix minor issues, still broken though

--- interpreter.mathematica/src/main/java/gov/nist/drmf/interpreter/mathematica/wrapper/ExprFormatException.java
+++ interpreter.mathematica/src/main/java/gov/nist/drmf/interpreter/mathematica/wrapper/ExprFormatException.java
@@ -1,8 +1,7 @@
 package gov.nist.drmf.interpreter.mathematica.wrapper;
 
 public class ExprFormatException extends Throwable {
-
     public ExprFormatException(com.wolfram.jlink.ExprFormatException e) {
-
+        super(e.getMessage(), e);
     }
 }
